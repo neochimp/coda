7c8a0661255a1d9f4f4d25b2397fcadb7eaded0d
Cameron Lee <7cameronlee@gmail.com>
2026-04-01 22:10:37 -0700

    Setup main for command line inputs

--- src/main.c
+++ src/main.c
@@ -1,53 +1,39 @@
 #include <stdio.h>
 #include <sqlite3.h>
 #include <unistd.h>
+#include <string.h>
 #include "commands.h"
-
-static int callback(void *NotUsed, int argc, char *argv[], char *azColName[]){
-  for (int i = 0; i<argc; i++){
-    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-  }
-  printf("\n");
-  return 0;
-}
 
 int main(int argc, char *argv[]){
   //Arguments:
   //-h, --help: description and flags available
   //-a, --album
-  //-A, --artist
+  //-r, --artist
   //-d, --date YYYY-MM-DD
   
- // if(argc < 2){
- //   fprintf(stderr, "Missing comand.\n");
- //   usage_info(argv[0]);
- //   return 1;
- // }
-  
+  if(argc < 2){
+    fprintf(stderr, "Missing comand.\n");
+    usage_info(argv[0]);
+    return 1;
+  }
 
-  //example quickstart from documentation
-/*
-  sqlite3 *db;
-  char *zErrMsg = 0;
-  int rc;
-
-  if( argc!= 3) {
-    fprintf(stderr, "Usage: %s DATABASE SQL-STATEMENT\n", argv[0]);
-    return(1);
+  if(strcmp(argv[1], "add") == 0){
+    cmd_add(argc, argv);
+  }else if(strcmp(argv[1], "list") == 0){
+    cmd_list();
+  }else if(strcmp(argv[1], "init") == 0){
+    if(argv[2]){
+      cmd_init(argv[2]);
+    }else{
+      cmd_init("coda.db");
+    }
+  }else if(strcmp(argv[1], "edit") == 0){
+    printf("edit not yet implemented");
+  }else if(strcmp(argv[1], "search") == 0){
+    printf("search not yet implemented");
+  }else if(strcmp(argv[1], "remove") == 0){
+    printf("remove not yet implemented");
   }
-  rc = sqlite3_open(argv[1], &db);
-  if (rc){
-    fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-    return(1);
-  }
-  rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
-  if( rc != SQLITE_OK){
-    fprintf(stderr, "SQL error: %s\n", zErrMsg);
-    sqlite3_free(zErrMsg);
-  }
-  sqlite3_close(db);
-*/
 
   return 0;
 }
